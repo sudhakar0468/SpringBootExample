208f109db63089a16e20fc4b00a1c7b2933a7934
sudhakarreddy pogula <s.pogula@anchorfree.com>
2022-04-08 15:47:25 +0530

    SringBootExample application Commit

--- src/main/java/com/mySpringBoot/SpringBootExample/SpringBootExampleApplication.java
+++ src/main/java/com/mySpringBoot/SpringBootExample/SpringBootExampleApplication.java
@@ -1,6 +1,6 @@
 package com.mySpringBoot.SpringBootExample;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
